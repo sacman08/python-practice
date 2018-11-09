a2fb2aefad7b287ecb4874f8d3d6c2af34de4b83
A. Sacman <44899859+sacman08@users.noreply.github.com>
2018-11-09 11:41:34 -0500

    Update Ex12ListEnds.py

--- Ex12ListEnds.py
+++ Ex12ListEnds.py
@@ -6,7 +6,9 @@
 #  www.praticepython.org Excerise 12 List Ends
 #  
 print("""
-Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) 
+and makes a new list of only the first and last elements of the given list. 
+For practice, write this code inside a function.
 """)
 import random
 
