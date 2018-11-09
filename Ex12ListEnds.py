2ef09466c653beea1aefa4c217b7cfc8a99260a5
A. Sacman <44899859+sacman08@users.noreply.github.com>
2018-11-09 11:40:54 -0500

    Update Ex12ListEnds.py

--- Ex12ListEnds.py
+++ Ex12ListEnds.py
@@ -9,13 +9,9 @@ print("""
 Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
 """)
 import random
-print ("")
+
 a = random.randint(1,15)
 b = random.sample(range(100), a)
-#def get_strend(a):
-#	y =[]
-#	y.append(a[0])
-#	y.append(a[-1])
-#	return a
+
 print ("This list of numbers is ", b)	
 print ("The start number is ", b[0], "and the end number is ", b[-1])
