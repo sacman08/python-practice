a68b9c27312a4ca3471a0e9e2bdf4ae129769c0f
sacman08 <44899859+sacman08@users.noreply.github.com>
2018-11-09 09:55:48 -0500

    Create SETaxCalc.py

---
+++ SETaxCalc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  SETaxCalc.py
+#  
+#  
+#  
+
+print ("""\
+Welcome to self employed tax rate calculator!
+Please have your miles driven, wages earned,
+and miscellanous cost totals ready for 2018!
+""")
+
+FedSETax = .153
+FedIncTax1 = .1
+FecIncTax2 = .12
+FedMileRate = .545
+GAIncTax = .06
+
+#Input requests
+WageEarned = float(input("What are the wages you earned?"))
+# Add a goto or jump to end operator if WageEarned is zero.
+MileEarned = float(input("How many miles were driven?"))
+MiscEarned = float(input("Please enter any other costs:"))
+
+# Compute routine
+# Add a if statement to findout if earns are over $9525
+TotalTax =  (WageEarned * FedSETax) + (WageEarned * FedIncTax1) + (WageEarned * GAIncTax)
+TotalMile = MileEarned * FedMileRate
+TotalDisc = TotalMile + MiscEarned
+
+print("For $",WageEarned," your total tax is $",format(TotalTax, '.2f'), ".")
+print('For ',MileEarned,' miles and $',MiscEarned,' your total writoff is $',TotalDisc,'.')
